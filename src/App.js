976b45c2b07b97b001684b8bfc35db7d1e3dde42
hamityay <hamityay@hotmail.com>
2020-01-01 20:11:11 +0300

    Convert App to component to use state and add show hide form fumctionality.

--- src/App.js
+++ src/App.js
@@ -7,18 +7,33 @@ import JobCreationForm from './components/job_creation_form';
 
 // const items = ['banana', 'apple', 'orange'];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">
-          Creating a Reusable List Component
-        </h1>
-      </header>
-      {/* <List items={jobs} itemElement={Jobıtem} /> */}
-      <JobCreationForm />
+class App extends React.Component {
+  state = { isFormVisible: true };
+
+  toggleFormVisible = () => {
+    this.setState({
+      isFormVisible: !this.state.isFormVisible
+    });
+  };
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">
+            Creating a Reusable List Component
+          </h1>
+        </header>
+        {/* <List items={jobs} itemElement={Jobıtem} /> */}
+        <button onClick={this.toggleFormVisible}>
+          {this.state.isFormVisible ? 'Hide Form' : 'Show Form'}
+        </button>
+        <div style={{visibility: this.state.isFormVisible ? 'visible' : 'hidden'}}>
+          <JobCreationForm />
+        </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
