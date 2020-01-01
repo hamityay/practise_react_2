63b10d0d9c98bebd18780cac8e6ee200a29041f2
hamityay <hamityay@hotmail.com>
2020-01-01 16:33:17 +0300

    Add counter component example.

---
+++ src/components/counter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './counter.css'
+
+export default class Counter extends React.Component {
+  state = {
+    counter: 0
+  };
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
+
+  render() {
+    return(
+      <div className="counter-box">
+        <button onClick={ this.increment }>+1</button>
+        { this.state.counter }
+        <button onClick={ this.decrement }>-1</button>
+      </div>
+    );
+  }
+}
